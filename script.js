3bf9888a569f6d995aa858453289d1267ea48363
ramadhanu27 <rhamadhanu2019@gmail.com>
2025-08-21 11:43:22 +0700

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+// Jam digital
+function updateClock() {
+  const now = new Date();
+  let h = now.getHours().toString().padStart(2, "0");
+  let m = now.getMinutes().toString().padStart(2, "0");
+  let s = now.getSeconds().toString().padStart(2, "0");
+  document.getElementById("digitalClock").textContent = `${h}:${m}:${s}`;
+}
+setInterval(updateClock, 1000);
+updateClock();
